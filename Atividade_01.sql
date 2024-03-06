debe663e9ffb58a6c3c505781cc224d83091ac92
JPxuares <143659489+JPxuares@users.noreply.github.com>
2024-03-05 21:19:26 -0300

    Add files via upload

---
+++ Atividade_01.sql
@@ -0,0 +1,16 @@
+create database SIS2402;
+create table CLIENTES (
+  nome varchar(50),
+  email varchar(50),
+  datanasc integer,
+  sexo varchar(50),
+  cep integer,
+  rua varchar(50),
+  numeroCasa integer,
+  bairro varchar(50),
+  cidade varchar(50),
+  uf varchar(50),
+  pais varchar(50)
+  );
+  
+  
